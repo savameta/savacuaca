26e3e66af2fd421ac20b69fd7203e5c01f6c133a
maru <hoando@savameta.com>
2024-02-07 09:20:12 +0700

    refactor: update backgorund

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button
-        type="button"
-        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-      >
-        Small
-      </button>
+    <main className="flex justify-center items-center ">
+      <div className="grid grid-cols-3 h-[671.82px] w-[1000px]">
+        <div className="hindi"></div>
+        <div className="ballot"></div>
+        <div className="chicken"></div>
+        <div className="fish"></div>
+        <div className="crab"></div>
+        <div className="shrimp"></div>
+      </div>
     </main>
   );
 }
